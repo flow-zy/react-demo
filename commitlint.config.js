5089c8f5d9b9da33630ef0da9158f530e5dc636e
zoujunjie <wz19121@yeah.net>
2025-01-25 14:34:35 +0800

    ci: 🎡 ska

--- commitlint.config.js
+++ commitlint.config.js
@@ -47,17 +47,17 @@ export default {
 		},
 		types: [
 			// 中文版
-			{ value: "feat", name: "特性:   🚀  新增功能", emoji: "🚀" },
-			{ value: "fix", name: "修复:   🧩  修复缺陷", emoji: "🧩" },
-			{ value: "docs", name: "文档:   📚  文档变更", emoji: "📚" },
-			{ value: "style", name: "格式:   🎨  代码格式（不影响功能，例如空格、分号等格式修正）", emoji: "🎨" },
-			{ value: "refactor", name: "重构:   ♻️  代码重构（不包括 bug 修复、功能新增）", emoji: "♻️" },
-			{ value: "perf", name: "性能:   ⚡️  性能优化", emoji: "⚡️" },
-			{ value: "test", name: "测试:   ✅  添加疏漏测试或已有测试改动", emoji: "✅" },
-			{ value: "build", name: "构建:   📦️  构建流程、外部依赖变更（如升级 npm 包、修改 webpack 配置等）", emoji: "📦️" },
-			{ value: "ci", name: "集成:   🎡  修改 CI 配置、脚本", emoji: "🎡" },
-			{ value: "chore", name: "回退:   ⏪️  回滚 commit", emoji: "⏪️" },
-			{ value: "chore", name: "其他:   🔨  对构建过程或辅助工具和库的更改（不影响源文件、测试用例）", emoji: "🔨" }
+			{ value: "feat", name: "feat:   🚀  新增功能", emoji: "🚀" },
+			{ value: "fix", name: "fix:   🧩  修复缺陷", emoji: "🧩" },
+			{ value: "docs", name: "docs:   📚  文档变更", emoji: "📚" },
+			{ value: "style", name: "style:   🎨  代码格式（不影响功能，例如空格、分号等格式修正）", emoji: "🎨" },
+			{ value: "refactor", name: "refactor:   ♻️  代码重构（不包括 bug 修复、功能新增）", emoji: "♻️" },
+			{ value: "perf", name: "perf:   ⚡️  性能优化", emoji: "⚡️" },
+			{ value: "test", name: "test:   ✅  添加疏漏测试或已有测试改动", emoji: "✅" },
+			{ value: "build", name: "build:   📦️  构建流程、外部依赖变更（如升级 npm 包、修改 webpack 配置等）", emoji: "📦️" },
+			{ value: "ci", name: "ci:   🎡  修改 CI 配置、脚本", emoji: "🎡" },
+			{ value: "chore", name: "chore:   ⏪️  回滚 commit", emoji: "⏪️" },
+			{ value: "revert", name: "revert:   🔨  对构建过程或辅助工具和库的更改（不影响源文件、测试用例）", emoji: "🔨" }
 		],
 		useEmoji: true,
 		themeColorCode: "",
