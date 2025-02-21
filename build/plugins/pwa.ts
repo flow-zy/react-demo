c92ef6cad5ba92a2a9ad8858b3809e28e70859b9
zoujunjie <wz19121@yeah.net>
2025-02-21 14:05:23 +0800

    build: 📦️ 重新配置pwa

--- build/plugins/pwa.ts
+++ build/plugins/pwa.ts
@@ -1,15 +1,26 @@
 import { VitePWA } from 'vite-plugin-pwa'
 export const createPwaPlugin = () => {
 	return VitePWA({
-		base: '/',
 		registerType: 'autoUpdate',
-		scope: '/',
-		includeAssets: ['favicon.ico'],
 		manifest: {
 			name: 'Vite-React-TS',
 			short_name: 'Vite-React-TS',
 			description: 'Vite-React-TS',
-			theme_color: '#ffffff'
+			theme_color: '#ffffff',
+			icons: [
+				{
+					src: '/vite.svg',
+					sizes: '192x192',
+					type: 'image/svg+xml'
+				}
+			],
+
+		},
+		devOptions: {
+			enabled: true
+		},
+		workbox: {
+			globPatterns: ['**/*.{js,css,html,ico,png,svg}'] // Workbox 缓存策略配置
 		}
 	})
 }
